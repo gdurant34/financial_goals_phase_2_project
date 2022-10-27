add1cc212f26250f50ad0b1d979bb7b2e27613ae
Grace Durant <gdurant34@gmail.com>
2022-10-26 17:36:05 -0700

    fixed navBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -14,7 +14,13 @@ function NavBar() {
 
   const activeStyle = {
     border: "solid #00D4FF 2px",
-    fontWeight: "bold"
+    fontWeight: "bold",
+    color: "#00D4FF",
+    padding: "10px 20px",
+    marginRight: "20px",
+    background: "#0D2E4E",
+    textDecoration: "inherit", /* no underline */
+    borderRadius: "10px"
   };
 
     return(
@@ -23,36 +29,25 @@ function NavBar() {
               Financial Goals
           </h1>
           <NavLink
-            to="/"
-            /* set "exact" so it knows to only set activeStyle when route is deeply equal to link */
-            exact
-            style={linkStyles}
-            /* add prop for activeStyle */
-            activeStyle={activeStyle}
-            >Home</NavLink>
+            to="/home"            
+            style={({ isActive }) => isActive ? activeStyle : linkStyles}
+            >Home
+            </NavLink>
 
         <NavLink
-            to="/account-summary"
-            exact
-            style={linkStyles}
-            activeStyle={activeStyle}
+            to="/account-summary"          
+            style={({ isActive }) => isActive ? activeStyle : linkStyles}
             >Account Summary</NavLink>
           
           <NavLink
-            to="/goals"
-            exact
-            style={linkStyles}
-            activeStyle={activeStyle}
+            to="/goals"            
+            style={({ isActive }) => isActive ? activeStyle : linkStyles}
             >Active Goals</NavLink>
 
         <NavLink
-            to="/completed-goals"
-            exact
-            style={linkStyles}
-            activeStyle={activeStyle}
+            to="/completed-goals"            
+            style={({ isActive }) => isActive ? activeStyle : linkStyles}
             >Completed Goals</NavLink>
-
-        
         </div>
     );
 }
